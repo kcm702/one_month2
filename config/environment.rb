f2fb03743ad1001bb855886f4c6c35380d6805d4
Kimberly M <kcm230@nyu.edu>
2014-04-05 16:47:38 -0400

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+OneMonth2::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OneMonth2::Application.config.session_store :cookie_store, key: '_one_month2_session'
